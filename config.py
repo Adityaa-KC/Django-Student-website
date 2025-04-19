360ef5bdec99aea192e527dab193d9ee5c3d2a62
Adityaa-kc <adityakumarchaudhary1623@gmail.com>
2025-04-19 22:55:13 +0530

    "Some Enhancements"

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SESSION_SECRET', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///university_admission.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/uploads')
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10MB max file size
